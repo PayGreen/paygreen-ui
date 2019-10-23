c76b1cf375aa152e3487aedd6bde740cd702c48e
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-10-23 17:07:26 +0200

    feat: import normalize in storybook iframe

--- src/shared/global.js
+++ src/shared/global.js
@@ -1,14 +1,17 @@
 import { createGlobalStyle, css } from 'styled-components';
+import { normalize } from 'styled-normalize';
 
 /**
  * Style global of the iframe which display component
  */
 export const bodyStyles = css`
     @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,700,800');
-    font-family: 'Open Sans', sans-serif;;
+    font-family: 'Open Sans', sans-serif;
 `;
 
 export const GlobalStyle = createGlobalStyle`
+    ${normalize}
+
     body {
         ${bodyStyles}
     }
