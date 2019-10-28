0d5c999f2b0359d54a5d55131fb8bb9213c320f9
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-10-28 15:41:49 +0100

    feat: style link

--- src/lib/Link/Link.stories.js
+++ src/lib/Link/Link.stories.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import Link from './Link';
+import { colorOptions, colorDefault } from '../../shared/const';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, text, radios } from '@storybook/addon-knobs';
 
 storiesOf('Link', module)
     .addDecorator(withKnobs)
     .add('Link', () => (
-        <Link
-        >
-            {text('Label', 'Link text')}
-        </Link>
+        <p>
+            <Link
+                color={radios('Color', colorOptions, colorDefault)}
+            >
+                {text('Label', 'Link text')}
+            </Link> curabitur congue varius ex et posuere. Maecenas tincidunt diam ut nisl porttitor scelerisque. Donec egestas elit dolor, quis eleifend ipsum sagittis ut. Proin molestie lorem et neque tempus tristique. Mauris finibus dui sem, nec suscipit mi porta a.
+        </p>
     ), {
         notes: 'Notes for link.',
     });
