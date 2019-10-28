f5494cfb563893209d7f8f3f670e9b80e0984c48
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-10-28 10:13:58 +0100

    refactor: split style files for button

---
+++ src/lib/Button/style/base.js
@@ -0,0 +1,74 @@
+import styled, {css} from 'styled-components';
+
+const ButtonBase = styled.a.attrs(props => ({
+    color: props.color || props.theme.color.primary.main,
+    size: props.size || 'md'
+}))`
+    display: inline-block;
+    position: relative;
+    outline: none;
+    cursor: ${props => props.type == 'disabled' ? 'not-allowed' : 'pointer'};
+    text-transform: uppercase;
+    font-weight: ${props => props.theme.font.weight.bold};
+    font-size: ${props => props.theme.button.font[props.size]};
+    letter-spacing: ${props => props.theme.button.letterSpacing[props.size]};
+    padding: ${props => props.theme.button.paddingHeight[props.size]} ${props => props.theme.button.paddingWidth[props.size]};
+    margin: ${props => props.theme.button.margin};
+    transition: all ${props => props.theme.transition.xs};
+
+    &::before,
+    &::after {
+        content: '';
+        display: block;
+        position: absolute;
+        z-index: ${props => props.theme.zindex.button};
+        height: 100%;
+        width: 100%;
+        border-radius: ${props => props.theme.radius.sm};
+        opacity: ${props => props.type == 'reverse' ? 0.6 : 0.5};
+        transition:
+            all ${props => props.theme.transition.xs},
+            opacity ${props => props.theme.transition.sm} linear ${props => props.theme.transition.xs};
+    }
+
+    &::before {
+        top: ${props => props.theme.button.shift};
+        left: ${props => props.theme.button.shift};
+    }
+
+    &::after {
+        bottom: ${props => props.theme.button.shift};
+        right: ${props => props.theme.button.shift};
+    }
+`;
+
+const hover = css`
+    &:hover,
+    &:active,
+    &:focus {
+        &::before,
+        &::after {
+            opacity: 1;
+        }
+
+        &::before {
+            top: 0;
+            left: 0;
+        }
+
+        &::after {
+            bottom: 0;
+            right: 0;
+        }
+    }
+`;
+
+const disabled = css`
+    cursor: 'not-allowed';
+`;
+
+export {
+    ButtonBase, 
+    hover,
+    disabled
+};
