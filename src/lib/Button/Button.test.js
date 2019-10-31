5351129125263a72acb15e0332ff23faf3e14773
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-10-31 17:07:40 +0100

    feat: test on breadcrumb component

--- src/lib/Button/Button.test.js
+++ src/lib/Button/Button.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Button from './Button';
-import TestRenderer from 'react-test-renderer'; // ES6
+import TestRenderer from 'react-test-renderer';
 import { ThemeDefault } from '../../theme';
 
 it('renders without crashing', () => {
