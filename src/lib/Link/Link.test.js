f7d4f47bcf00cd2d46036ec26cbb53c8c0d69c75
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-10-31 17:22:40 +0100

    feat: add tests on Link component

---
+++ src/lib/Link/Link.test.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from './Link';
+import TestRenderer from 'react-test-renderer';
+import { ThemeDefault } from '../../theme';
+
+it('renders without crashing', () => {
+    const link = TestRenderer.create(
+        <a href="#">
+            <Link theme={ThemeDefault}>
+                Link text
+            </Link>
+        </a>
+    );
+    expect(link.toJSON()).toMatchSnapshot();
+})
