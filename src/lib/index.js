f6c6d9b409150c275e7e3e63770aa612478a9def
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-11-13 14:31:29 +0100

    feat: add missing component calls in index.js

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,15 +1,19 @@
 import Breadcrumb from './Breadcrumb/Breadcrumb';
 import Button from './Button/Button';
 import ButtonGroup from './ButtonGroup/ButtonGroup';
+import Card from './Card/Card';
 import Input from './Input/Input';
 import Link from './Link/Link';
+import Textarea from './Textarea/Textarea';
 import Title from './Title/Title';
 
 export {
     Breadcrumb,
     Button,
     ButtonGroup,
+    Card,
     Input,
     Link,
+    Textarea,
     Title
 };
