7c8d4f1f1b98e8d393a17de91273975c7afe2b11
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-11-15 17:22:13 +0100

    feat: add optionnal param coeff in calculateSpace function in order to adapt margin-bottom in ButtonGroup component

--- src/lib/ButtonGroup/style/constants.js
+++ src/lib/ButtonGroup/style/constants.js
@@ -1,12 +1,19 @@
 import { math } from 'polished';
 
+const bottomCoeff = 0.75;
+
 const buttonSpace = props => props.resetMargin ? props.theme.space.sm : 0;
 
-const calculateSpace = (space) => {
-    return props => math(props.theme.blockPadding[space] + '-' + buttonSpace(props));
+const calculateSpace = (space, coeff) => {
+    if (coeff === undefined) {
+        coeff = 1;
+    }
+
+    return props => math(props.theme.blockPadding[space] + '*' + coeff + '-' + buttonSpace(props));
 };
 
 export {
+    bottomCoeff,
     buttonSpace,
     calculateSpace
 };
