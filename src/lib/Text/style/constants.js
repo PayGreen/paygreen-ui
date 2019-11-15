4fa84f3931ef36f1a5cc29fe32dce09add63f6d5
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-11-15 17:21:17 +0100

    feat: rewrite blockSpace function in order to accept both constants and values in pixels

--- src/lib/Text/style/constants.js
+++ src/lib/Text/style/constants.js
@@ -1,15 +1,18 @@
 import { stripUnit } from 'polished';
+import { blockPaddingOptions } from '../../../shared/constants';
 
 const blockSpace = (screen, space) => {
+    const size = blockPaddingOptions.hasOwnProperty(space) ? props => props.theme.blockPadding[space] : space;
+
     switch (screen) {
         case 'xs':
             return props => props.theme.blockPadding.xs;
         case 'sm':
-            return props => stripUnit(props.theme.blockPadding[space]) > stripUnit(props.theme.blockPadding.xs) ?
+            return props => stripUnit(size(props)) > stripUnit(props.theme.blockPadding.xs) ?
                 props.theme.blockPadding.sm :
                 props.theme.blockPadding.xs;
         case 'md':
-            return props => props.theme.blockPadding[space];
+            return props => size(props);
     }
 }
 
