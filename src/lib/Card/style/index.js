544825b38d1d08326ffcc371e6def1071e1e2893
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-11-15 15:12:19 +0100

    fix: style card need false padding (in order to contain margin of children)

--- src/lib/Card/style/index.js
+++ src/lib/Card/style/index.js
@@ -7,6 +7,7 @@ import {
 const CardBase = styled.div`
     ${props => props.hasBackground ? backgroundStyle[props.colorType] : null};
     max-width: ${props => props.theme.blockWidth[props.blockWidth]};
+    padding: 0.1px;
     transition: all ${props => props.theme.transition.sm};
 `;
 
