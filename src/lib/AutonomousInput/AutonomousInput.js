bd1893ce9208bfe90382679eb79230c8b478d039
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-11-22 17:27:38 +0100

    feat: props in AutonomousInput

--- src/lib/AutonomousInput/AutonomousInput.js
+++ src/lib/AutonomousInput/AutonomousInput.js
@@ -1,24 +1,57 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import {
-
+    formStatusOptions,
+    formStatusDefault,
 } from '../../shared/constants';
 import { AutonomousInputBase } from './style';
 
 class AutonomousInput extends PureComponent {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            value: props.value !== undefined ? props.value : '',
+        };
+        
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
     render() {
-        return <AutonomousInputBase {...this.props}>
-            {this.props.children}
+        const {
+            status,
+            submittedButton,
+            children,
+            ...rest
+        } = this.props;
+
+        return <AutonomousInputBase
+            theme={this.props.theme} // not necessary, only needed for tests
+            status={status}
+        >
+            <input
+                {...rest}
+                value={this.state.value}
+                onChange={this.handleChange}
+            />
+
+            {children}
         </AutonomousInputBase>;
     }
 }
 
 AutonomousInput.propTypes = {
-
+    status: PropTypes.oneOf(Object.values(formStatusOptions)),
+    submittedButton: PropTypes.string
 };
 
 AutonomousInput.defaultProps = {
-
+    status: formStatusDefault,
+    submittedButton: null
 };
 
 export default AutonomousInput;
--- src/lib/AutonomousInput/AutonomousInput.stories.js
+++ src/lib/AutonomousInput/AutonomousInput.stories.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import AutonomousInput from './AutonomousInput';
 import {
-
+    formStatusOptions,
+    formStatusDefault
 } from '../../shared/constants';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean, text, radios, select } from '@storybook/addon-knobs';
+import { withKnobs, text, radios } from '@storybook/addon-knobs';
 
 storiesOf('AutonomousInput', module)
     .addDecorator(withKnobs)
     .add('AutonomousInput', () => (
         <AutonomousInput
-            
+            status={radios('Status', formStatusOptions, formStatusDefault)}
+            placeholder={text('Placeholder', 'Your email...')}
+            submittedButton={text('Submitted button label', 'Received!')}
+            type="email"
         >
-            <input type="email" placeholder="Your email..." />
-
-            <button type="button">Send</button>
+            <button type="submit">
+                {text('Default button label', 'Send')}
+            </button>
         </AutonomousInput>
     ), {
         notes: 'AutonomousInput.',
