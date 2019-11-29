62edf420ebc5a9b87de145774ccfba2bc7e7ec3f
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-11-29 17:57:29 +0100

    feat: base style MenuList

--- src/lib/MenuList/style/index.js
+++ src/lib/MenuList/style/index.js
@@ -1,7 +1,15 @@
 import styled from 'styled-components';
 
 const MenuListBase = styled.ul`
+    margin: 0;
+    padding: ${props => props.theme.space.sm} 0;
+    border-top: solid ${props => props.theme.line} ${props => props.theme.wab.white20};
 
+    li {
+        list-style-type: none;
+        margin: ${props => props.theme.line} 0;
+        padding: 0;
+    }
 `;
 
 export { MenuListBase };
