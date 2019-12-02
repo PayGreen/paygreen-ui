22672e8ac6d155e8e0f7e08a9ad4dc5f7d8b87ec
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-12-02 10:49:23 +0100

    feat: add prop types for MenuList

--- src/lib/MenuList/MenuList.js
+++ src/lib/MenuList/MenuList.js
@@ -1,7 +1,8 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import {
-
+    colorThemeOptions,
+    colorThemeDefault,
 } from '../../shared/constants';
 import { MenuListBase } from './style';
 
@@ -20,11 +21,13 @@ class MenuList extends PureComponent {
 }
 
 MenuList.propTypes = {
-
+    isOpen: PropTypes.bool,
+    colorTheme: PropTypes.oneOf(Object.values(colorThemeOptions)),
 };
 
 MenuList.defaultProps = {
-
+    isOpen: true,
+    colorTheme: colorThemeDefault,
 };
 
 export default MenuList;
