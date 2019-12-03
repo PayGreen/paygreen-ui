e0216e001e5b002dbc31a53c1248bf63c1cef1e2
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-12-03 10:36:47 +0100

    feat: add tests for MenuHamburger

---
+++ src/lib/MenuHamburger/MenuHamburger.test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+import { ThemeDefault } from '../../theme';
+import MenuHamburger from './MenuHamburger';
+
+it('renders without crashing', () => {
+    const menuHamburger = TestRenderer.create(
+        <MenuHamburger
+            theme={ThemeDefault}
+        />
+    );
+    expect(menuHamburger.toJSON()).toMatchSnapshot();
+});
