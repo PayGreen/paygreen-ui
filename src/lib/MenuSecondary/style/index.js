740235c5bffd0dd0750d398889a4910b86970d10
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-12-03 16:45:55 +0100

    feat: add shadow in MenuSecondary

--- src/lib/MenuSecondary/style/index.js
+++ src/lib/MenuSecondary/style/index.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { transparentize } from 'polished';
 import { menuBlock } from '../../MenuPrimary/style/base';
 import { closedStyle } from './base';
 
@@ -8,12 +9,16 @@ const MenuSecondaryBase = styled.ul`
     @media (${props => props.theme.query.max.lg}) {
         max-height: calc(100vh - ${props => props.theme.grid.header});
         padding: ${props => props.theme.space.sm} 0;
+        box-shadow: 0 3px 20px ${props => transparentize(
+            props.theme.shadow.opacity.sm,
+            props.theme.wab.black00
+        )};
         
         ${props => props.isOpen ? null : closedStyle};
     }
 
     @media (${props => props.theme.query.min.lg}) {
-        
+
     }
 `;
 
