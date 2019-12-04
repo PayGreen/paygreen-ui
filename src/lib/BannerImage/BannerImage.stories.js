3dca0ce05388910555377b0cbc2bcf2e91f35ab4
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-12-04 22:01:04 +0100

    feat: add contents in BannerImage story

--- src/lib/BannerImage/BannerImage.stories.js
+++ src/lib/BannerImage/BannerImage.stories.js
@@ -5,18 +5,49 @@ import {
     colorThemeOptions,
     colorThemeDefault,
     waveOptions,
-    waveDefault
+    waveDefault,
+    colorNumberOptions,
+    greyOptions,
+    fontSizeOptions,
+    blockSpaceOptions,
+    blockWidthDefault
 } from '../../shared/constants';
+import Card from '../Card/Card';
+import Title from '../Title/Title';
+import Text from '../Text/Text';
 import BannerImage from './BannerImage';
 import photoFile from './sample/sample.png';
+
+const colorThemeLabel = 'ColorTheme';
 
 storiesOf('BannerImage', module).addDecorator(withKnobs)
 .add('BannerImage', () => (
     <BannerImage
-        colorTheme={radios('Color theme', colorThemeOptions, colorThemeDefault)}
+        colorTheme={radios(colorThemeLabel, colorThemeOptions, colorThemeDefault)}
         topStyle={radios('Wave top direction', waveOptions, waveDefault)}
         image={photoFile}
     >
-        Sample
+        <Card
+            hasBackground={false}
+            blockWidth={blockWidthDefault}
+        >
+            <Title
+                colorNumber={colorNumberOptions.two}
+                colorTheme={radios(colorThemeLabel, colorThemeOptions, colorThemeDefault)}
+                color2={greyOptions.grey60}
+                textSize={fontSizeOptions.lg}
+                marginLateral={blockSpaceOptions.md}
+                marginTop={blockSpaceOptions.md}
+            >
+                Your title with <strong>an emphasis</strong>
+            </Title>
+
+            <Text
+                marginLateral={blockSpaceOptions.md}
+                marginTop={blockSpaceOptions.md}
+            >
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis porttitor velit a ultricies aliquet. Donec vehicula in arcu non sodales. Fusce et consectetur odio. Ut bibendum ullamcorper turpis vel imperdiet. 
+            </Text>
+        </Card>
     </BannerImage>
 ));
