2eb996b831362c822164b84fda56a57dd94fcd06
Matthieu Coulon <matthieu.coulon@paygreen.fr>
2019-12-04 17:19:56 +0100

    core : update index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,3 @@
-import * as Icon from './Icon/components';
-
 import AutonomousInput from './AutonomousInput/AutonomousInput';
 import Breadcrumb from './Breadcrumb/Breadcrumb';
 import Button from './Button/Button';
@@ -21,17 +19,102 @@ import Text from './Text/Text';
 import Textarea from './Textarea/Textarea';
 import Title from './Title/Title';
 
+// Icons
+import { ArrowBottomIcon } from './Icon/Icon';
+import { ArrowCircleLeftIcon } from './Icon/Icon';
+import { ArrowCircleLeftBoldIcon } from './Icon/Icon';
+import { ArrowCircleRightBoldIcon } from './Icon/Icon';
+import { ArrowCircleRightIcon } from './Icon/Icon';
+import { ArrowLeftIcon } from './Icon/Icon';
+import { ArrowLongBottomIcon } from './Icon/Icon';
+import { ArrowLongLeftIcon } from './Icon/Icon';
+import { ArrowLongRightIcon } from './Icon/Icon';
+import { ArrowLongTopIcon } from './Icon/Icon';
+import { ArrowRightIcon } from './Icon/Icon';
+import { ArrowRoundBottomLeftIcon } from './Icon/Icon';
+import { ArrowRoundBottomRightIcon } from './Icon/Icon';
+import { ArrowRoundTopLeftIcon } from './Icon/Icon';
+import { ArrowRoundTopRightIcon } from './Icon/Icon';
+import { ArrowTopIcon } from './Icon/Icon';
+import { BalloonBoldIcon } from './Icon/Icon';
+import { BalloonIcon } from './Icon/Icon';
+import { BankIcon } from './Icon/Icon';
+import { BeakerBoldIcon } from './Icon/Icon';
+import { BeakerIcon } from './Icon/Icon';
+import { BellBoldIcon } from './Icon/Icon';
+import { BellIcon } from './Icon/Icon';
+import { BulbBoldIcon } from './Icon/Icon';
+import { BulbIcon } from './Icon/Icon';
+import { CardsBoldIcon } from './Icon/Icon';
+import { CardsIcon } from './Icon/Icon';
+import { CarIcon } from './Icon/Icon';
+import { CartIcon } from './Icon/Icon';
+import { CautionBoldIcon } from './Icon/Icon';
+import { CautionIcon } from './Icon/Icon';
+import { CheckBoldIcon } from './Icon/Icon';
+import { CheckboxIcon } from './Icon/Icon';
+import { CheckIcon } from './Icon/Icon';
+import { ClockBoldIcon } from './Icon/Icon';
+import { ClockIcon } from './Icon/Icon';
+import { CodeIcon } from './Icon/Icon';
+import { CoinsIcon } from './Icon/Icon';
+import { ContractIcon } from './Icon/Icon';
+import { CrossBoldIcon } from './Icon/Icon';
+import { CrossIcon } from './Icon/Icon';
+import { DownloadIcon } from './Icon/Icon';
+import { EmptyIcon } from './Icon/Icon';
+import { EyeIcon } from './Icon/Icon';
+import { GlobeIcon } from './Icon/Icon';
+import { HeartIcon } from './Icon/Icon';
+import { HourglassIcon } from './Icon/Icon';
+import { InformationIcon } from './Icon/Icon';
+import { LeafIcon } from './Icon/Icon';
+import { LetterBoldIcon } from './Icon/Icon';
+import { LetterIcon } from './Icon/Icon';
+import { LinkBoldIcon } from './Icon/Icon';
+import { LinkIcon } from './Icon/Icon';
+import { LockIcon } from './Icon/Icon';
+import { MealIcon } from './Icon/Icon';
+import { MenuBoldIcon } from './Icon/Icon';
+import { MenuIcon } from './Icon/Icon';
+import { MeterIcon } from './Icon/Icon';
+import { OrganizationIcon } from './Icon/Icon';
+import { OutIcon } from './Icon/Icon';
+import { PaperclipIcon } from './Icon/Icon';
+import { PenIcon } from './Icon/Icon';
+import { PhoneIcon } from './Icon/Icon';
+import { PictureIcon } from './Icon/Icon';
+import { PlaneIcon } from './Icon/Icon';
+import { PointerIcon } from './Icon/Icon';
+import { PopinIcon } from './Icon/Icon';
+import { QuestionBoldIcon } from './Icon/Icon';
+import { QuestionIcon } from './Icon/Icon';
+import { QuoteLeftIcon } from './Icon/Icon';
+import { QuoteRightIcon } from './Icon/Icon';
+import { RefreshBoldIcon } from './Icon/Icon';
+import { RefreshIcon } from './Icon/Icon';
+import { SatchelIcon } from './Icon/Icon';
+import { ScheduleIcon } from './Icon/Icon';
+import { SearchBoldIcon } from './Icon/Icon';
+import { SearchIcon } from './Icon/Icon';
+import { ShopIcon } from './Icon/Icon';
+import { TreesIcon } from './Icon/Icon';
+import { TrophyIcon } from './Icon/Icon';
+import { TruckIcon } from './Icon/Icon';
+import { UserBoldIcon } from './Icon/Icon';
+import { UsersIcon } from './Icon/Icon';
+import { UserIcon } from './Icon/Icon';
+import { WalletIcon } from './Icon/Icon';
+import { WheelsIcon } from './Icon/Icon';
+
 // theme
 import { ThemeDefault, ThemeDark } from '../theme'
-import { ThemeProvider as PGThemeProvider, withTheme } from 'styled-components'
-import { GlobalStyle } from '../shared/global';
+import { ThemeProvider as PGThemeProvider } from 'styled-components'
 
 export {
-    GlobalStyle,
     ThemeDefault,
     ThemeDark,
     PGThemeProvider,
-    Icon,
     AutonomousInput,
     Breadcrumb,
     Button,
@@ -52,4 +135,91 @@ export {
     Text,
     Textarea,
     Title,
+    // Icons
+    ArrowBottomIcon,
+    ArrowCircleLeftIcon,
+    ArrowCircleLeftBoldIcon,
+    ArrowCircleRightBoldIcon,
+    ArrowCircleRightIcon,
+    ArrowLeftIcon,
+    ArrowLongBottomIcon,
+    ArrowLongLeftIcon,
+    ArrowLongRightIcon,
+    ArrowLongTopIcon,
+    ArrowRightIcon,
+    ArrowRoundBottomLeftIcon,
+    ArrowRoundBottomRightIcon,
+    ArrowRoundTopLeftIcon,
+    ArrowRoundTopRightIcon,
+    ArrowTopIcon,
+    BalloonBoldIcon,
+    BalloonIcon,
+    BankIcon,
+    BeakerBoldIcon,
+    BeakerIcon,
+    BellBoldIcon,
+    BellIcon,
+    BulbBoldIcon,
+    BulbIcon,
+    CardsBoldIcon,
+    CardsIcon,
+    CarIcon,
+    CartIcon,
+    CautionBoldIcon,
+    CautionIcon,
+    CheckBoldIcon,
+    CheckboxIcon,
+    CheckIcon,
+    ClockBoldIcon,
+    ClockIcon,
+    CodeIcon,
+    CoinsIcon,
+    ContractIcon,
+    CrossBoldIcon,
+    CrossIcon,
+    DownloadIcon,
+    EmptyIcon,
+    EyeIcon,
+    GlobeIcon,
+    HeartIcon,
+    HourglassIcon,
+    InformationIcon,
+    LeafIcon,
+    LetterBoldIcon,
+    LetterIcon,
+    LinkBoldIcon,
+    LinkIcon,
+    LockIcon,
+    MealIcon,
+    MenuBoldIcon,
+    MenuIcon,
+    MeterIcon,
+    OrganizationIcon,
+    OutIcon,
+    PaperclipIcon,
+    PenIcon,
+    PhoneIcon,
+    PictureIcon,
+    PlaneIcon,
+    PointerIcon,
+    PopinIcon,
+    QuestionBoldIcon,
+    QuestionIcon,
+    QuoteLeftIcon,
+    QuoteRightIcon,
+    RefreshBoldIcon,
+    RefreshIcon,
+    SatchelIcon,
+    ScheduleIcon,
+    SearchBoldIcon,
+    SearchIcon,
+    ShopIcon,
+    TreesIcon,
+    TrophyIcon,
+    TruckIcon,
+    UserBoldIcon,
+    UsersIcon,
+    UserIcon,
+    WalletIcon,
+    WheelsIcon,
 };
