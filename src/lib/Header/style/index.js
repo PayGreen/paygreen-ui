aab41bb3fef6c7e576f80901360459b6a0681b42
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-12-04 15:22:56 +0100

    feat: style Header with logo feat: style MenuSecondary in Header

--- src/lib/Header/style/index.js
+++ src/lib/Header/style/index.js
@@ -1,16 +1,58 @@
 import styled from 'styled-components';
 import { headerStyle } from './base';
+import { MenuPrimaryBase } from '../../MenuPrimary/style';
+import { MenuSecondaryBase } from '../../MenuSecondary/style';
+import { MenuHamburgerBase } from '../../MenuHamburger/style';
+import { LogoBase } from '../../Logo/style';
 
 const HeaderBase = styled.header`
-    .nav-buttons {
-        ${headerStyle};
+    a {
+        height: 100%;
+    }
+
+    ${LogoBase} {
+        height: 100%;
+        width: 120px;
+    }
+
+    @media (${props => props.theme.query.max.lg}) {
+        .main-nav {
+            ${headerStyle};
+        }
+
+        ${LogoBase} {
+            padding: 0 !important;
+        }
     }
 
     @media (${props => props.theme.query.min.lg}) {
         ${headerStyle};
 
-        .nav-buttons {
+        ${MenuHamburgerBase},
+        .icon {
             display: none;
+        }
+
+        ${MenuPrimaryBase} {
+            flex: 1;
+            justify-content: flex-end;
+        }
+
+        ${MenuSecondaryBase} {
+            margin-left: ${props => props.theme.space.lg};
+            padding-left: ${props => props.theme.space.lg};
+
+            &:before {
+                content: '';
+                position: absolute;
+                transform: translate(-50%, -50%);
+                top: 50%;
+                left: 0;
+                width: 12px;
+                height: 12px;
+                border-radius: 50%;
+                background-color: ${props => props.theme.wab.grey10};
+            }
         }
     }
 `;
