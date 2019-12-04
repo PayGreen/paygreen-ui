beb4ae7568b1a2311d2ee77645e26a3f10377cff
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-12-04 18:50:35 +0100

    feat: clean Banner component (only gradient, not image)

--- src/lib/Banner/Banner.js
+++ src/lib/Banner/Banner.js
@@ -1,11 +1,8 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import {
-    gradientOptions,
     colorThemeOptions,
     colorThemeDefault,
-    maskOptions,
-    maskDefault,
 } from '../../shared/constants';
 import { BannerBase } from './style';
 
@@ -20,17 +17,11 @@ class Banner extends PureComponent {
 }
 
 Banner.propTypes = {
-    gradient: PropTypes.oneOf(Object.values(gradientOptions)),
     colorTheme: PropTypes.oneOf(Object.values(colorThemeOptions)),
-    topStyle: PropTypes.oneOf(Object.values(maskOptions)),
-    bottomStyle: PropTypes.oneOf(Object.values(maskOptions)),
 };
 
 Banner.defaultProps = {
-    gradient: gradientOptions.theme,
     colorTheme: colorThemeDefault,
-    topStyle: maskDefault,
-    bottomStyle: maskDefault,
 };
 
 export default Banner;
