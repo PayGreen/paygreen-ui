1c4995b76b976acf9171c4784e8a0c18a4335567
Matthieu Coulon <matthieu.coulon@paygreen.fr>
2019-12-04 16:23:19 +0100

    feat: add Menu to index

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,4 +1,4 @@
-import Icon from './Icon/components'; 
+import Icon from './Icon/components';
 
 import AutonomousInput from './AutonomousInput/AutonomousInput';
 import Breadcrumb from './Breadcrumb/Breadcrumb';
@@ -8,9 +8,14 @@ import Card from './Card/Card';
 import IconLabel from './IconLabel/IconLabel';
 import Image from './Image/Image';
 import Input from './Input/Input';
+import Header from './Header/Header';
 import Link from './Link/Link';
+import MenuClose from './MenuClose/MenuClose';
+import MenuHamburger from './MenuHamburger/MenuHamburger';
 import MenuItem from './MenuItem/MenuItem';
 import MenuList from './MenuList/MenuList';
+import MenuPrimary from './MenuPrimary/MenuPrimary';
+import MenuSecondary from './MenuSecondary/MenuSecondary';
 import Select from './Select/Select';
 import Text from './Text/Text';
 import Textarea from './Textarea/Textarea';
@@ -34,10 +39,14 @@ export {
     Card,
     IconLabel,
     Image,
-    Input,
+    Header,
     Link,
+    MenuClose,
+    MenuHamburger,
     MenuItem,
     MenuList,
+    MenuPrimary,
+    MenuSecondary,
     Select,
     Text,
     Textarea,
