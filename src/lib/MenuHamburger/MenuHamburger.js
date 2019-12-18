01ae08cbbaca21283bf38fd8062ae2049f17ddae
Matthieu Coulon <matthieu.coulon@paygreen.fr>
2019-12-18 16:57:16 +0100

    feat: update menuHamburger and simplify

--- src/lib/MenuHamburger/MenuHamburger.js
+++ src/lib/MenuHamburger/MenuHamburger.js
@@ -1,45 +1,25 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
-import {
-    colorThemeOptions,
-    colorThemeDefault
-} from '../../shared/constants';
-import { MenuHamburgerBase } from './style';
+import React from "react";
+import PropTypes from "prop-types";
+import { colorThemeOptions, colorThemeDefault } from "../../shared/constants";
+import { MenuHamburgerBase } from "./style";
 
-class MenuHamburger extends PureComponent {
-    constructor(props) {
-        super(props);
-
-        this.state = {
-            isOpen: false,
-        };
-        
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState({isOpen: !this.state.isOpen});
-    }
-
-    render() {
-        return <MenuHamburgerBase
-            {...this.props}
-            isOpen={this.state.isOpen}
-            onClick={this.handleClick}
-        >
-            <span></span>
-            <span></span>
-            <span></span>
-        </MenuHamburgerBase>;
-    }
-}
+const MenuHamburger = props => (
+    <MenuHamburgerBase
+        {...props}
+        isOpen={props.isOpen}
+    >
+        <span></span>
+        <span></span>
+        <span></span>
+    </MenuHamburgerBase>
+);
 
 MenuHamburger.propTypes = {
-    colorTheme: PropTypes.oneOf(Object.values(colorThemeOptions)),
+  colorTheme: PropTypes.oneOf(Object.values(colorThemeOptions))
 };
 
 MenuHamburger.defaultProps = {
-    colorTheme: colorThemeDefault,
+  colorTheme: colorThemeDefault
 };
 
 export default MenuHamburger;
