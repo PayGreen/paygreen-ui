17d810eda20c0e9efd5973e79483717ef29f6287
Olivia Gometz <olivia.gometz@paygreen.fr>
2019-12-27 16:49:07 +0100

    feat: update and create tests for List and ListItem component

--- src/lib/List/List.test.js
+++ src/lib/List/List.test.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { ThemeDefault } from '../../theme';
+import Text from '../Text/Text';
+import ListItem from '../ListItem/ListItem';
 import List from './List';
 
 it('renders without crashing', () => {
     const component = TestRenderer.create(
         <List theme={ThemeDefault}>
-            Something
+            <ListItem theme={ThemeDefault}>
+                <Text theme={ThemeDefault}>
+                    Something
+                </Text>
+            </ListItem>
+
+            <ListItem theme={ThemeDefault}>
+                <Text theme={ThemeDefault}>
+                    Something else
+                </Text>
+            </ListItem>
         </List>
     );
     expect(component.toJSON()).toMatchSnapshot();
