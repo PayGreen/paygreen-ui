5f43dda86fcd8aa983a7cf3df83fa0c72f8a0e23
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-01-06 10:00:08 +0100

    fix: useless CSS and constant name in Banner style

--- src/lib/Banner/style/index.js
+++ src/lib/Banner/style/index.js
@@ -5,16 +5,14 @@ import { topWaveStyle, bottomWaveStyle } from './base';
 
 const BannerBase = styled.div`
     position: relative;
-    margin-top: ${props => props.topStyle !== decorationOptions.none ? props.theme.wave.double : 0};
-    margin-bottom: ${props => props.bottomStyle !== decorationOptions.none ? props.theme.wave.double : 0};
     padding: .1px 0;
     background-image: linear-gradient(
         ${props => color.first[props.gradient]},
         ${props => color.second[props.gradient]}
     );
 
-    ${props => props.topStyle !== waveOptions.none ? topWaveStyle : null};
-    ${props => props.bottomStyle !== waveOptions.none ? bottomWaveStyle : null};
+    ${props => props.topStyle !== decorationOptions.none ? topWaveStyle : null};
+    ${props => props.bottomStyle !== decorationOptions.none ? bottomWaveStyle : null};
 
     .waves {
         position: absolute;
