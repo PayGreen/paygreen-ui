1aa5c7e6779a50d46c103359f69ac4bad5d44426
MattMattC <roadfen@gmail.com>
2020-01-08 14:27:11 +0100

    feat: try  extract loading stories to  order by theme

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
-import * as  themes from '../src/theme';
+import * as themes from '../src/theme';
 import { configure, addDecorator, addParameters } from '@storybook/react';
 import { withThemesProvider } from 'storybook-addon-styled-component-theme';
 import { withA11y } from '@storybook/addon-a11y';
@@ -7,44 +7,51 @@ import { create } from '@storybook/theming';
 import { GlobalStyle } from '../src/shared/global';
 import React from 'react';
 
-addDecorator(withThemesProvider(Object.keys(themes).map((k) => themes[k])));
+addDecorator(withThemesProvider(Object.keys(themes).map(k => themes[k])));
 addDecorator(withA11y);
 
 addParameters({
-  viewport: {
-    viewports: INITIAL_VIEWPORTS,
-  },
-  // style of the board storyBook
-  options: {
-    // https://storybook.js.org/docs/configurations/theming/
-    theme: create({
-      brandTitle: 'PayGreen',
-      base: 'light',
-      appBg: '#f9f9f9',
-    }),
-    isFullscreen: false,
-    showPanel: true,
-  },
-  backgrounds: [
-    // https://www.npmjs.com/package/@storybook/addon-backgrounds
-    { name: 'White', value: '#ffffff', default: true },
-    { name: 'Primary', value: '#77d1a6' },
-    { name: 'PrimaryShade', value: '#a4db95' },
-    { name: 'Secondary', value: '#51c6be' },
-    { name: 'SecondaryShade', value: '#76bbd8' },
-    { name: 'Tertiary', value: '#fab56b' },
-    { name: 'TertiaryShade', value: '#faa272' },
-    { name: 'Quaternary', value: '#e98e90' },
-    { name: 'QuaternaryShade', value: '#e98eb9' },
-    { name: 'DarkMode', value: '#202020' },
-  ],
+    viewport: {
+        viewports: INITIAL_VIEWPORTS,
+    },
+    // style of the board storyBook
+    options: {
+        // https://storybook.js.org/docs/configurations/theming/
+        theme: create({
+            brandTitle: 'PayGreen',
+            base: 'light',
+            appBg: '#f9f9f9',
+        }),
+        isFullscreen: false,
+        showPanel: true,
+    },
+    backgrounds: [
+        // https://www.npmjs.com/package/@storybook/addon-backgrounds
+        { name: 'White', value: '#ffffff', default: true },
+        { name: 'Primary', value: '#77d1a6' },
+        { name: 'PrimaryShade', value: '#a4db95' },
+        { name: 'Secondary', value: '#51c6be' },
+        { name: 'SecondaryShade', value: '#76bbd8' },
+        { name: 'Tertiary', value: '#fab56b' },
+        { name: 'TertiaryShade', value: '#faa272' },
+        { name: 'Quaternary', value: '#e98e90' },
+        { name: 'QuaternaryShade', value: '#e98eb9' },
+        { name: 'DarkMode', value: '#202020' },
+    ],
 });
 
 addDecorator(story => (
-  <>
-    <GlobalStyle />
-    {story()}
-  </>
+    <>
+        <GlobalStyle />
+        {story()}
+    </>
 ));
 
-configure(require.context('../src', true, /\.stories\.(js|mdx)$/), module);
+const loadConfigures = () => {
+    // TODO : try to order 
+    const storiesMdx = require.context('../src', true, /\.stories\.mdx$/);
+    const storiesJSX = require.context('../src', true, /\.stories\.js$/);
+    return [storiesMdx, storiesJSX];
+};
+
+configure(loadConfigures(), module);
