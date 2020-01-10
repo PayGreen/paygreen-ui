22259eb6f02f8406fd3bfbec4c1dd940687c8a1a
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-01-10 15:08:04 +0100

    feat: update ListHorizontal tests

--- src/lib/ListHorizontal/ListHorizontal.test.js
+++ src/lib/ListHorizontal/ListHorizontal.test.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { ThemeDefault } from '../../theme';
+import Text from '../Text/Text';
 import ListHorizontal from './ListHorizontal';
 
 it('renders without crashing', () => {
     const component = TestRenderer.create(
         <ListHorizontal theme={ThemeDefault}>
-            Something
+            <Text theme={ThemeDefault}>
+                Text 1
+            </Text>
+
+            <Text theme={ThemeDefault}>
+                Text 2
+            </Text>
+
+            <Text theme={ThemeDefault}>
+                Text 3
+            </Text>
         </ListHorizontal>
     );
     expect(component.toJSON()).toMatchSnapshot();
