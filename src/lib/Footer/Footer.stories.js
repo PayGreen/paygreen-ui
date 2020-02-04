71d8df939c9481f8fac62fdccd32d5a23d5bd632
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-02-04 10:54:08 +0100

    feat: complete Footer stories

--- src/lib/Footer/Footer.stories.js
+++ src/lib/Footer/Footer.stories.js
@@ -6,7 +6,39 @@ import {
     decorationOptions,
     decorationDefault
 } from '../../shared/constants';
+import Grid from '../Grid/Grid';
+import Title from '../Title/Title';
+import Text from '../Text/Text';
+import Link from '../Link/Link';
 import Footer from './Footer';
+
+const sampleContents = [
+    {
+        title: 'Title 1',
+        links: [
+            'Link 1',
+            'Second link',
+            'Other link'
+        ]
+    },
+    {
+        title: 'Title two',
+        links: [
+            'New link',
+            'Page with long title',
+            'Link 78',
+            'Fourth link'
+        ]
+    },
+    {
+        title: 'Third title',
+        links: [
+            'Little link',
+            'Link',
+            'Final link'
+        ]
+    }
+];
 
 storiesOf(folder.template + 'Footer', module)
     .addDecorator(withKnobs)
@@ -20,7 +52,48 @@ storiesOf(folder.template + 'Footer', module)
                     decorationDefault,
                 )}
             >
-                Sample
+                <Grid childrenFlex={1}>
+                    {sampleContents.map((sample, index) => (
+                        <div key={index}>
+                            <Title
+                                align="center"
+                                textSize="xs"
+                                colorWab="grey30"
+                                hasUnderline={true}
+                                marginBottom="xs"
+                            >
+                                {sample.title}
+                            </Title>
+
+                            <ul>
+                                {sample.links.map((link, index) => (
+                                    <li key={index}>
+                                        <a href="#">
+                                            <Link
+                                                hasUnderline={false}
+                                                colorPallet="wab"
+                                                colorWab="grey50"
+                                            >
+                                                {link}
+                                            </Link>
+                                        </a>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    ))}
+                </Grid>
+
+                <Text
+                    align="center"
+                    textSize="xxs"
+                    colorWab="grey30"
+                    marginTop="xs"
+                    marginBottom="xs"
+                    marginInternal="none"
+                >
+                    PayGreen UI library – 2019
+                </Text>
             </Footer>
         )
     );
