dfa04431f589781d8a2e90665dfc786d725a959a
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-04-08 15:42:37 +0200

    refacto: remove useless attribute xmlns in some svg files

--- src/lib/Footer/style/waveTop.js
+++ src/lib/Footer/style/waveTop.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-const waveTop = <svg className="waveTop" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 998.8 81">
+const waveTop = <svg className="waveTop" viewBox="0 0 998.8 81">
     <path d="M999.3,0v80.5c0,0-201.7-65.8-498.9-50.5C208,47.9-0.7,0-0.7,0H999.3z"/>
 </svg>;
 
