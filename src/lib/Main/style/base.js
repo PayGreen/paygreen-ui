c3de48a5fc52655a0f6da5138dceffd136b6ff0d
fanya3 <fanya3@gmail.com>
2020-04-30 17:50:55 +0200

    feature: added box shadow

--- src/lib/Main/style/base.js
+++ src/lib/Main/style/base.js
@@ -1,10 +1,16 @@
 import { css } from 'styled-components';
+import { transparentize } from 'polished';
 
 const mainStyle = css`
     position: relative;
     z-index: ${props => props.theme.zindex.header};
     height: 100vh;
     background-color: ${props => props.theme.wab.white00};
+    margin-left: 20px;
+    box-shadow: ${props =>
+        props.theme.shadow.size.lg +
+        ' ' +
+        transparentize(props.theme.shadow.opacity.sm, props.theme.wab.black00)};
 `;
 
 const isClosedStyle = css`
