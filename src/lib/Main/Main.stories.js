e569a16dd63cacb5d76231b02f8573b2be47f095
fanya3 <fanya3@gmail.com>
2020-05-05 16:53:38 +0200

    feature: cleaned topbar file

--- src/lib/Main/Main.stories.js
+++ src/lib/Main/Main.stories.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
-import { folder, spaceOptions } from '../../shared/constants';
+import { folder } from '../../shared/constants';
 import Main from './Main';
 import Topbar from '../Topbar/Topbar';
 import MenuHamburger from '../MenuHamburger/MenuHamburger';
-import Logo from '../Logo/Logo';
-import { logo } from '../Logo/sample/logo';
 import { Box } from './style';
 
 const mainMenuIsOpenLabel = 'Open sidebar';
@@ -17,15 +15,6 @@ storiesOf(folder.nav + 'Main', module)
         <Main isOpen={boolean(mainMenuIsOpenLabel, true)}>
             <Topbar>
                 <MenuHamburger isOpen={boolean(mainMenuIsOpenLabel, true)} />
-                <a href="#">
-                    <Logo
-                        hasBaseline={false}
-                        hasHoverRight={true}
-                        blockWidth={spaceOptions.sm}
-                    >
-                        {logo}
-                    </Logo>
-                </a>
             </Topbar>
             <Box />
         </Main>
--- src/lib/Topbar/Topbar.stories.js
+++ src/lib/Topbar/Topbar.stories.js
@@ -1,25 +1,14 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
-import { folder, spaceOptions } from '../../shared/constants';
+import { folder } from '../../shared/constants';
 import MenuHamburger from '../MenuHamburger/MenuHamburger';
-import Logo from '../Logo/Logo';
 import Topbar from './Topbar';
-import { logo } from '../Logo/sample/logo';
 
 storiesOf(folder.nav + 'Topbar', module)
     .addDecorator(withKnobs)
     .add('Topbar', () => (
         <Topbar>
             <MenuHamburger isOpen={boolean('Is open', true)} />
-            <a href="#">
-                <Logo
-                    hasBaseline={false}
-                    hasHoverRight={true}
-                    blockWidth={spaceOptions.sm}
-                >
-                    {logo}
-                </Logo>
-            </a>
         </Topbar>
     ));
