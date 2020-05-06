6b28e2c780a75831e26feb4d962b328dfef46dae
fanya3 <fanya3@gmail.com>
2020-05-06 14:35:12 +0200

    feature: created main folder for sidebar menu

---
+++ src/lib/SIdebarMenu/SidebarMenu.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { itemHtmlTagOptions, itemHtmlTagDefault } from '../../shared/constants';
+import { SidebarMenuBase } from './style';
+
+const SidebarMenu = props => {
+    return (
+        <SidebarMenuBase as={props.htmlTag} {...props}>
+            {props.children}
+        </SidebarMenuBase>
+    );
+};
+
+SidebarMenu.propTypes = {
+    htmlTag: PropTypes.oneOf(Object.values(itemHtmlTagOptions)),
+};
+
+SidebarMenu.defaultProps = {
+    htmlTag: itemHtmlTagDefault,
+};
+
+export default SidebarMenu;
