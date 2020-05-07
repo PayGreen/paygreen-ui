09ffbd10bcb2cae823ad53b6415f4b5a82ab1ab4
fanya3 <fanya3@gmail.com>
2020-05-07 15:14:11 +0200

    feature:cleaned css code

--- src/lib/Sidebar/style/base.js
+++ src/lib/Sidebar/style/base.js
@@ -5,9 +5,11 @@ const sidebarStyle = css`
     z-index: ${props => props.theme.zindex.menu};
     height: 100vh;
     width: 100%;
+
     @media (${props => props.theme.query.min.md}) {
         width: ${props => props.theme.grid.sidebar};
     }
+    
     transition: all ${props => props.theme.transition.sm};
 `;
 
