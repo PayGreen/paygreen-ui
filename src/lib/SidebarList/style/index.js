a46c27bbb79a8d991bdc458a11d28ce4fa640c57
fanya3 <fanya3@gmail.com>
2020-05-07 14:50:56 +0200

    feature: cleaned css code of base.js for sidebarMenu

--- src/lib/SidebarList/style/index.js
+++ src/lib/SidebarList/style/index.js
@@ -5,31 +5,6 @@ const SidebarListBase = styled.ul`
     margin: 0;
     padding: 0;
 
-    @media (${props => props.theme.query.min.lg}) {
-        position: absolute;
-        width: ${props => props.theme.blockWidth.sm};
-
-        &::before {
-            content: '';
-            position: absolute;
-            top: -${props => props.theme.iconSize.md};
-            left: 50%;
-            transform: translateX(-50%);
-            border-right: solid ${props => props.theme.iconSize.md} transparent;
-            border-bottom: solid ${props => props.theme.iconSize.md} ${props => props.theme.wab.white00};
-            border-left: solid ${props => props.theme.iconSize.md} transparent;
-        }
-
-        &::after {
-            content: '';
-            position: absolute;
-            top: -${props => props.theme.iconSize.lg};
-            left: 0;
-            height: ${props => props.theme.iconSize.lg};
-            width: 100%;
-        }
-    }
-
     li {
         list-style-type: none;
         margin: ${props => props.theme.line} 0;
