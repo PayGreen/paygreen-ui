87a26e03070706e400de4fcc627326b43fe2119f
Pierrick LEFEBVRE <pierricklefebvre.pro@gmail.com>
2020-05-18 15:44:28 +0200

    feat: Icon deleted from test feat: Test's snapshot updated

--- src/lib/Badge/Badge.test.js
+++ src/lib/Badge/Badge.test.js
@@ -2,19 +2,8 @@ import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { ThemeDefault } from '../../theme';
 import Badge from './Badge';
-import { ArrowBottomIcon } from '../Icon/Icon';
 
 it('renders without crashing', () => {
-    const badge = TestRenderer.create(
-        <Badge theme={ThemeDefault} gradient="brand" textSize="sm">
-            Test
-            <ArrowBottomIcon
-                theme={ThemeDefault}
-                colorPallet="wab"
-                colorWab="white00"
-                iconSize="xs"
-            />
-        </Badge>,
-    );
+    const badge = TestRenderer.create(<Badge theme={ThemeDefault}>Test</Badge>);
     expect(badge.toJSON()).toMatchSnapshot();
 });
