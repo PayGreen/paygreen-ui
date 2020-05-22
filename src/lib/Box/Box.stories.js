c27d5918aab076cf1dcc2e407689d3b293fc3b41
Pierrick LEFEBVRE <pierricklefebvre.pro@gmail.com>
2020-05-22 14:49:02 +0200

    refacto: style/index.js is now prettier feat: use of select instead of text input for stories with custom spaceOptions (includes 'null')

--- src/lib/Box/Box.stories.js
+++ src/lib/Box/Box.stories.js
@@ -1,39 +1,109 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, text } from '@storybook/addon-knobs';
+import { withKnobs, select } from '@storybook/addon-knobs';
 import { folder } from '../../shared/constants';
 import Box from './Box';
 import Card from '../Card/Card';
 import Title from '../Title/Title';
 
+const spaceOptions = {
+    null: null,
+    none: 'none',
+    xs: 'xs',
+    sm: 'sm',
+    md: 'md',
+    lg: 'lg',
+    xl: 'xl',
+};
+
 storiesOf(folder.main + 'Box', module)
     .addDecorator(withKnobs)
     .add('Box', () => (
         <Box
-            padding={text('Padding', '')}
-            paddingTop={text('Padding top', '')}
-            paddingRight={text('Padding right', '')}
-            paddingLeft={text('Padding left', '')}
-            paddingBottom={text('Padding bottom', '')}
-            margin={text('Margin', '')}
-            marginTop={text('Margin top', '')}
-            marginRight={text('Margin right', '')}
-            marginLeft={text('Margin left', '')}
-            marginBottom={text('Margin bottom', '')}
+            padding={select('Padding', spaceOptions, spaceOptions.none)}
+            paddingTop={select('Padding top', spaceOptions, spaceOptions.null)}
+            paddingRight={select(
+                'Padding right',
+                spaceOptions,
+                spaceOptions.null,
+            )}
+            paddingLeft={select(
+                'Padding left',
+                spaceOptions,
+                spaceOptions.null,
+            )}
+            paddingBottom={select(
+                'Padding bottom',
+                spaceOptions,
+                spaceOptions.null,
+            )}
+            margin={select('Margin', spaceOptions, spaceOptions.none)}
+            marginTop={select('Margin top', spaceOptions, spaceOptions.null)}
+            marginRight={select(
+                'Margin right',
+                spaceOptions,
+                spaceOptions.null,
+            )}
+            marginLeft={select('Margin left', spaceOptions, spaceOptions.null)}
+            marginBottom={select(
+                'Margin bottom',
+                spaceOptions,
+                spaceOptions.null,
+            )}
         >
             <Card>
                 <Title align="center">Card</Title>
                 <Box
-                    padding={text('Inner padding', '')}
-                    paddingTop={text('Inner padding top', '')}
-                    paddingRight={text('Inner padding right', '')}
-                    paddingLeft={text('Inner padding left', '')}
-                    paddingBottom={text('Inner padding bottom', '')}
-                    margin={text('Inner margin', '')}
-                    marginTop={text('Inner margin top', '')}
-                    marginRight={text('Inner margin right', '')}
-                    marginLeft={text('Inner margin left', '')}
-                    marginBottom={text('Inner margin bottom', '')}
+                    padding={select(
+                        'Inner padding',
+                        spaceOptions,
+                        spaceOptions.none,
+                    )}
+                    paddingTop={select(
+                        'Inner padding top',
+                        spaceOptions,
+                        spaceOptions.null,
+                    )}
+                    paddingRight={select(
+                        'Inner padding right',
+                        spaceOptions,
+                        spaceOptions.null,
+                    )}
+                    paddingLeft={select(
+                        'Inner padding left',
+                        spaceOptions,
+                        spaceOptions.null,
+                    )}
+                    paddingBottom={select(
+                        'Inner padding bottom',
+                        spaceOptions,
+                        spaceOptions.null,
+                    )}
+                    margin={select(
+                        'Inner margin',
+                        spaceOptions,
+                        spaceOptions.none,
+                    )}
+                    marginTop={select(
+                        'Inner margin top',
+                        spaceOptions,
+                        spaceOptions.null,
+                    )}
+                    marginRight={select(
+                        'Inner margin right',
+                        spaceOptions,
+                        spaceOptions.null,
+                    )}
+                    marginLeft={select(
+                        'Inner margin left',
+                        spaceOptions,
+                        spaceOptions.null,
+                    )}
+                    marginBottom={select(
+                        'Inner margin bottom',
+                        spaceOptions,
+                        spaceOptions.null,
+                    )}
                 >
                     <Card>
                         <Title align="center">Inner Card</Title>
--- src/lib/Box/style/index.js
+++ src/lib/Box/style/index.js
@@ -2,50 +2,50 @@ import styled from 'styled-components';
 
 const BoxBase = styled.div`
     max-width: 100%;
-    
-    padding:${props => props.theme.space[props.padding]}
-    
-    padding-top:${props =>
+
+    padding: ${props => props.theme.space[props.padding]};
+
+    padding-top: ${props =>
         props.paddingTop
             ? props.theme.space[props.paddingTop]
-            : props.theme.space[props.padding]}
-    
-    padding-right:${props =>
+            : props.theme.space[props.padding]};
+
+    padding-right: ${props =>
         props.paddingRight
             ? props.theme.space[props.paddingRight]
-            : props.theme.space[props.padding]}
-    
-    padding-left:${props =>
+            : props.theme.space[props.padding]};
+
+    padding-left: ${props =>
         props.paddingLeft
             ? props.theme.space[props.paddingLeft]
-            : props.theme.space[props.padding]}
-    
-    padding-bottom:${props =>
+            : props.theme.space[props.padding]};
+
+    padding-bottom: ${props =>
         props.paddingBottom
             ? props.theme.space[props.paddingBottom]
-            : props.theme.space[props.padding]}
+            : props.theme.space[props.padding]};
 
-    margin:${props => props.theme.space[props.margin]}
-    
-    margin-top:${props =>
+    margin: ${props => props.theme.space[props.margin]};
+
+    margin-top: ${props =>
         props.marginTop
             ? props.theme.space[props.marginTop]
-            : props.theme.space[props.margin]}
-    
-    margin-right:${props =>
+            : props.theme.space[props.margin]};
+
+    margin-right: ${props =>
         props.marginRight
             ? props.theme.space[props.marginRight]
-            : props.theme.space[props.margin]}
-    
-    margin-left:${props =>
+            : props.theme.space[props.margin]};
+
+    margin-left: ${props =>
         props.marginLeft
             ? props.theme.space[props.marginLeft]
-            : props.theme.space[props.margin]}
-    
-    margin-bottom:${props =>
+            : props.theme.space[props.margin]};
+
+    margin-bottom: ${props =>
         props.marginBottom
             ? props.theme.space[props.marginBottom]
-            : props.theme.space[props.margin]}
+            : props.theme.space[props.margin]};
 `;
 
 export { BoxBase };
