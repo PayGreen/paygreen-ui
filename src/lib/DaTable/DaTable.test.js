6bfb1b786f1ed562e1471ea6f4ef9fd9fdf298d9
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-05-28 16:40:27 +0200

    feat: write tests for Table and Row

--- src/lib/DaTable/DaTable.test.js
+++ src/lib/DaTable/DaTable.test.js
@@ -1,13 +1,35 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { ThemeDefault } from '../../theme';
+import DaTableCell from '../DaTableCell/DaTableCell';
+import DaTableRow from '../DaTableRow/DaTableRow';
 import DaTable from './DaTable';
+
+const row = (
+    <DaTableRow theme={ThemeDefault}>
+        <DaTableCell theme={ThemeDefault} isCheckbox={true}>
+            <input type="checkbox" />
+        </DaTableCell>
+
+        <DaTableCell theme={ThemeDefault} isId={true}>
+            4217
+        </DaTableCell>
+
+        <DaTableCell theme={ThemeDefault}>Something</DaTableCell>
+
+        <DaTableCell theme={ThemeDefault} isMain={false}>
+            Something else
+        </DaTableCell>
+    </DaTableRow>
+);
 
 it('renders without crashing', () => {
     const component = TestRenderer.create(
         <DaTable theme={ThemeDefault}>
-            Something
-        </DaTable>
+            {row}
+            {row}
+            {row}
+        </DaTable>,
     );
     expect(component.toJSON()).toMatchSnapshot();
 });
