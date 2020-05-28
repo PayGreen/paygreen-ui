825092351ec4242e7aee43e08dd709f469f93984
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-05-28 16:39:09 +0200

    refacto: remove useless imports in DaTable

--- src/lib/DaTable/DaTable.js
+++ src/lib/DaTable/DaTable.js
@@ -1,24 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
-import {
-
-} from '../../shared/constants';
 import { DaTableBase } from './style';
 
-const DaTable = (props) => {
-    return <DaTableBase
-        {...props}
-    >
-        {props.children}
-    </DaTableBase>;
-};
-
-DaTable.propTypes = {
-
-};
-
-DaTable.defaultProps = {
-
+const DaTable = props => {
+    return <DaTableBase {...props}>{props.children}</DaTableBase>;
 };
 
 export default DaTable;
