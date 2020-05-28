e9f9e4213aef4acf2e57b605cefe8ef97273d1ac
fanya3 <fanya3@gmail.com>
2020-05-28 17:22:02 +0200

    feature: switch from class to functional component and updated test

--- src/lib/DaInput/style/base.js
+++ src/lib/DaInput/style/base.js
@@ -1,30 +1,23 @@
 import { css } from 'styled-components';
 import { math } from 'polished';
-import {
-    fieldTextColor,
-} from './constants';
 
 const field = css`
-    display: block;
     box-sizing: border-box;
+    display: block;
     outline: none;
     border-style: solid;
-    border-width:${props => props.theme.line};
+    border-width: ${props => props.theme.line};
     border-color: ${props => props.theme.wab.grey10};
     border-radius: ${props =>
         math(props.theme.daButton.buttonHeight[props.fieldSize] + '/2')};
-    padding: 0 ${props =>
-        math(props.theme.daButton.buttonHeight[props.fieldSize] + '/2')};
+    padding: 0
+        ${props =>
+            math(props.theme.daButton.buttonHeight[props.fieldSize] + '/2')};
     height: ${props => props.theme.daButton.buttonHeight[props.fieldSize]};
     width: 100%;
     font-size: ${props => props.theme.daButton.font[props.fieldSize]};
+    color: ${props => props.theme.wab.grey60};
     transition: all ${props => props.theme.transition.sm};
-`;
-
-const disabledField = css`
-    cursor: not-allowed;
-    color: ${props => props.theme.wab.grey40};
-    background-color: ${props => props.theme.wab.white20};
 `;
 
 const canHover = css`
@@ -32,35 +25,19 @@ const canHover = css`
     &:active,
     &:focus {
         border-color: ${props => props.theme.color.status.default.main};
-        padding-left: 3em;
-        padding-right: 3em;
-
-        & + span {
-            &::before,
-            &::after {
-                opacity: 1;
-            }
-        }
     }
 `;
 
 const enabled = css`
-    color: ${props => fieldTextColor[props.status]};
     background-color: ${props => props.theme.wab.white00};
 
-    & + span {
-        &::before,
-        &::after {
-            background-color: ${props => props.theme.color.status[props.status].main};
-            opacity: .7;
-        }
-    }
-
-    ${props => props.inputReadOnly ? null : canHover};
+    ${props => (props.inputReadOnly ? null : canHover)};
 `;
 
 const disabled = css`
-    ${disabledField};
+    cursor: not-allowed;
+    color: ${props => props.theme.wab.grey40};
+    background-color: ${props => props.theme.wab.white20};
 `;
 
 const helpButtonStyle = css`
@@ -69,10 +46,4 @@ const helpButtonStyle = css`
     border-bottom-right-radius: 0;
 `;
 
-export {
-    field,
-    disabledField,
-    enabled,
-    disabled,
-    helpButtonStyle,
-};
+export { field, disabled, enabled, helpButtonStyle };
--- src/lib/DaInput/style/index.js
+++ src/lib/DaInput/style/index.js
@@ -1,31 +1,22 @@
 import styled from 'styled-components';
-import { responsiveSpaces } from '../../../shared/spaces';
-import {
-    field,
-    enabled,
-    disabled, helpButtonStyle
-} from './base';
+import { field, enabled, disabled, helpButtonStyle } from './base';
 
 const DaInputBase = styled.div`
-    ${responsiveSpaces('margin')};
-
-    max-width: ${props => props.inputType === 'tel' ?
-        props.theme.form.inputWidth.sm :
-        props.theme.form.inputWidth[props.fieldSize]
-    };
+    max-width: ${props =>
+        props.inputType === 'tel'
+            ? props.theme.form.inputWidth.sm
+            : props.theme.form.inputWidth[props.blockWidth]};
 
     input {
         ${field};
-        ${props => props.inputDisabled ? disabled : enabled};
+        ${props => (props.inputDisabled ? disabled : enabled)};
 
         ::placeholder {
             color: ${props => props.theme.wab.grey30};
             font-style: italic;
         }
-        ${props => props.hasHelpButton ? helpButtonStyle : null};
+        ${props => (props.hasHelpButton ? helpButtonStyle : null)};
     }
-
-
 `;
 
 export { DaInputBase };
