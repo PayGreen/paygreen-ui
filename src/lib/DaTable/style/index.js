30540de7774b105212f698bd3b5cb2d846972467
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-05-28 16:30:35 +0200

    feat: base style for Table

--- src/lib/DaTable/style/index.js
+++ src/lib/DaTable/style/index.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components';
 
 const DaTableBase = styled.div`
-
+    @media (${props => props.theme.query.min.lg}) {
+        display: table;
+        background-color: ${props => props.theme.wab.white10};
+    }
 `;
 
 export { DaTableBase };
