1c89e8471b70b992ff2296f17f75e01af4749135
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-06-04 12:13:41 +0200

    feat: complete DaTableHead storie

--- src/lib/DaTableHead/DaTableHead.stories.js
+++ src/lib/DaTableHead/DaTableHead.stories.js
@@ -1,35 +1,41 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean, text, radios, select } from '@storybook/addon-knobs';
-import {
-    folder,
-} from '../../shared/constants';
-import DaTableCell from '../DaTableCell/DaTableCell';
+import { withKnobs } from '@storybook/addon-knobs';
+import { folder } from '../../shared/constants';
+import { ArrowBottomIcon, MenuIcon } from '../Icon/Icon';
+import DaTableHeadCell from '../DaTableHeadCell/DaTableHeadCell';
 import DaTableHead from './DaTableHead';
 
 storiesOf(folder.tables + folder.sub.daTable + 'DaTableHead', module)
     .addDecorator(withKnobs)
-    .add(
-        'DaTableHead',
-        () => (
-            <DaTableHead
-                
+    .add('DaTableHead', () => (
+        <DaTableHead>
+            <DaTableHeadCell isCheckbox={true}>
+                <input type="checkbox" style={{ display: 'block' }} />
+            </DaTableHeadCell>
+
+            <DaTableHeadCell
+                sortIcon={<ArrowBottomIcon title="Sort DESC on Sample" />}
+                groupIcon={<MenuIcon title="Group on Sample" />}
             >
-                <DaTableCell>
-                    Sample
-                </DaTableCell>
+                Sample
+            </DaTableHeadCell>
 
-                <DaTableCell>
-                    Sample
-                </DaTableCell>
+            <DaTableHeadCell>Sample</DaTableHeadCell>
 
-                <DaTableCell>
-                    Sample
-                </DaTableCell>
+            <DaTableHeadCell
+                sortIcon={
+                    <ArrowBottomIcon
+                        isActive={true}
+                        title="Sort ASC on Sample"
+                    />
+                }
+            >
+                Sample
+            </DaTableHeadCell>
 
-                <DaTableCell>
-                    Sample
-                </DaTableCell>
-            </DaTableHead>
-        )
-    );
+            <DaTableHeadCell>Sample</DaTableHeadCell>
+
+            <DaTableHeadCell>Sample</DaTableHeadCell>
+        </DaTableHead>
+    ));
