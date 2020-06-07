d07aa5271382124dacaf2f33cc76459ddb599565
fanya3 <fanya3@gmail.com>
2020-06-07 11:56:53 +0200

    feature: added color mode disabled, readOnly

--- src/lib/DaHelp/style/base.js
+++ src/lib/DaHelp/style/base.js
@@ -1,27 +1,46 @@
 import { css } from 'styled-components';
 
-const enabled = css`
+const activeStyle = css`
+    background-image: linear-gradient(
+        to bottom right,
+        ${props => props.theme.color.secondary.gradientBase},
+        ${props => props.theme.color.secondary.gradientShade}
+    );
+    & .icon {
+        svg {
+            fill: ${props => props.theme.wab.white00};
+        }
+    }
+`;
+
+const readOnly = css`
+    cursor: not-allowed;
+`;
+
+const canHover = css`
     &:hover,
     &:active,
     &:focus {
-        background-color:blue;
+        ${activeStyle}
     }
-    ${props => (props.readOnly ? readOnly : null)};
-    ${props => (props.isActive ? isActive : null)};
+`;
+
+const enabled = css`
+    background-color: ${props => props.theme.color.status.default.light};
+
+    & .icon {
+        svg {
+            fill: ${props => props.theme.color.status.default.main};
+        }
+    }
+    ${props => (props.isActive ? activeStyle : null)};
+    ${props => (props.readOnly ? readOnly : canHover)};
 }
 `;
 
 const disabled = css`
     cursor: not-allowed;
     filter: grayscale(1);
-`;
-
-const isActive = css`
-    background-color:green;
-`;
-
-const readOnly = css`
-    cursor: not-allowed;
 `;
 
 export { enabled, disabled };
--- src/lib/DaHelp/style/index.js
+++ src/lib/DaHelp/style/index.js
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { math } from 'polished';
-import { enabled, disabled } from './base';
+import { enabled, disabled, activeStyle } from './base';
 
 const DaHelpBase = styled.button`
     box-sizing: border-box;
@@ -9,24 +9,30 @@ const DaHelpBase = styled.button`
             math(props.theme.daButton.buttonHeight[props.fieldSize] + '/3')}
         0
         ${props =>
-            math(props.theme.daButton.buttonHeight[props.fieldSize] + '/4')};
-    height: ${props => props.theme.daButton.buttonHeight[props.fieldSize]};
-    border: solid ${props => props.theme.line}
-        ${props => props.theme.color.status.default.light};
+            math(props.theme.daButton.buttonHeight[props.fieldSize] + '/5')};
+    height: ${props =>
+        math(
+            props.theme.daButton.buttonHeight[props.fieldSize] +
+                '+' +
+                props.theme.line +
+                '*2',
+        )};
     border-radius: ${props =>
-        math(props.theme.daButton.buttonHeight[props.fieldSize] + '/2')};
+        math(
+            props.theme.daButton.buttonHeight[props.fieldSize] +
+                '+' +
+                props.theme.line +
+                '*2' +
+                '/2',
+        )};
     border-left: none;
     border-top-left-radius: 0;
     border-bottom-left-radius: 0;
-    background-color: red;
     transition: all ${props => props.theme.transition.xs};
 
     & .icon {
         width: ${props => props.theme.iconSize[props.fieldSize]};
         height: auto;
-        svg {
-            fill: ${props => props.theme.color.status.default.main};
-        }
     }
 
     ${props => (props.disabled ? disabled : enabled)};
