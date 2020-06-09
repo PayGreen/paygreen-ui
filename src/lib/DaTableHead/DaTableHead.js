259c008d603a28ee7eba8309510d912bad85a0c4
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-06-09 09:55:48 +0200

    refacto: use functionnal component instead of class component on DaTableHead

--- src/lib/DaTableHead/DaTableHead.js
+++ src/lib/DaTableHead/DaTableHead.js
@@ -1,4 +1,4 @@
-import React, { PureComponent } from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import {
     fontSizeOptions,
@@ -14,65 +14,49 @@ import Text from '../Text/Text';
 import { ArrowBottomIcon } from '../Icon/Icon';
 import { DaTableHeadBase } from './style';
 
-class DaTableHead extends PureComponent {
-    constructor(props) {
-        super(props);
+const DaTableHead = props => {
+    const [headOpen, setHeadOpen] = useState(false);
 
-        this.state = {
-            headOpen: false,
-        };
-
-        this.handleClick = this.handleClick.bind(this);
-    }
-
-    handleClick() {
-        this.setState({ headOpen: !this.state.headOpen });
-    }
-
-    render() {
-        return (
-            <DaTableHeadBase {...this.props} isHeadOpen={this.state.headOpen}>
-                <InternalGrid
-                    theme={this.props.theme} // not necessary, only needed for tests
-                    alignItems={alignItemsOptions.center}
-                    onClick={this.handleClick}
+    return (
+        <DaTableHeadBase {...props} isHeadOpen={headOpen}>
+            <InternalGrid
+                theme={props.theme} // not necessary, only needed for tests
+                alignItems={alignItemsOptions.center}
+                onClick={() => setHeadOpen(!headOpen)}
+            >
+                <Title
+                    theme={props.theme} // not necessary, only needed for tests
+                    textSize={fontSizeOptions.xxs}
+                    colorWab={greyOptions.grey50}
                 >
-                    <Title
-                        theme={this.props.theme} // not necessary, only needed for tests
-                        textSize={fontSizeOptions.xxs}
-                        colorWab={greyOptions.grey50}
-                    >
-                        {this.props.blockTitle}
-                    </Title>
+                    {props.blockTitle}
+                </Title>
 
-                    <ArrowBottomIcon
-                        theme={this.props.theme} // not necessary, only needed for tests
-                        iconSize={iconSizeOptions.md}
-                        colorPallet={colorPalletOptions.wab}
-                        colorWab={greyOptions.grey60}
-                        rotateSize={
-                            this.state.headOpen
-                                ? rotateSizeOptions.d180
-                                : rotateSizeOptions.d0
-                        }
-                    />
-                </InternalGrid>
+                <ArrowBottomIcon
+                    theme={props.theme} // not necessary, only needed for tests
+                    iconSize={iconSizeOptions.md}
+                    colorPallet={colorPalletOptions.wab}
+                    colorWab={greyOptions.grey60}
+                    rotateSize={
+                        headOpen ? rotateSizeOptions.d180 : rotateSizeOptions.d0
+                    }
+                />
+            </InternalGrid>
 
-                {this.props.children}
+            {props.children}
 
-                {this.props.resultsLabel ? (
-                    <Text
-                        theme={this.props.theme} // not necessary, only needed for tests
-                        textSize={fontSizeOptions.xs}
-                        align={alignItemsOptions.center}
-                    >
-                        {this.props.resultsLabel}
-                    </Text>
-                ) : null}
-            </DaTableHeadBase>
-        );
-    }
-}
+            {props.resultsLabel ? (
+                <Text
+                    theme={props.theme} // not necessary, only needed for tests
+                    textSize={fontSizeOptions.xs}
+                    align={alignItemsOptions.center}
+                >
+                    {props.resultsLabel}
+                </Text>
+            ) : null}
+        </DaTableHeadBase>
+    );
+};
 
 DaTableHead.propTypes = {
     blockTitle: PropTypes.string,
