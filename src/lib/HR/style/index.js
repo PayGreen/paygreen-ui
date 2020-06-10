5f184da794990b79216d953b6e734ee2c541c58c
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-06-10 11:02:34 +0200

    fix: margins for HR

--- src/lib/HR/style/index.js
+++ src/lib/HR/style/index.js
@@ -1,21 +1,12 @@
 import styled from 'styled-components';
-import { directionalProperty } from 'polished';
 import { responsiveSpaces } from '../../../shared/spaces';
 import { colorTypeOptions } from '../../../shared/constants';
 import { color } from './constants';
 
 const HRBase = styled.div`
-    ${responsiveSpaces('margin')};
-
-    ${props =>
-        directionalProperty(
-            'margin',
-            props.theme.space[props.marginTop],
-            responsiveSpaces('margin'),
-            props.theme.space[props.marginBottom],
-            responsiveSpaces('margin'),
-        )};
-
+    ${responsiveSpaces('margin', 0, 1, 1, ['Lateral'])};
+    margin-top: ${props => props.theme.space[props.marginTop]};
+    margin-bottom: ${props => props.theme.space[props.marginBottom]};
     border: none;
     background: ${props =>
         props.colorType === colorTypeOptions.reverse
