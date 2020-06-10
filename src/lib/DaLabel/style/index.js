1020616c6ef10dfc27b229baf245109ead2e4f2d
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-06-10 11:29:03 +0200

    refacto: use constant instead of '4px' :p

--- src/lib/DaLabel/style/index.js
+++ src/lib/DaLabel/style/index.js
@@ -6,9 +6,10 @@ const DaLabelBase = styled.label`
     color: ${props => props.theme.wab.grey30};
     font-size: ${props => props.theme.daButton.font[props.fieldSize]};
     font-weight: ${props => props.theme.font.weight.bold};
-    padding: 4px
-        ${props =>
-            math(props.theme.daButton.buttonHeight[props.fieldSize] + '/2')};
+    padding: ${props =>
+        props.theme.space.xs +
+        ' ' +
+        math(props.theme.daButton.buttonHeight[props.fieldSize] + '/2')};
 `;
 
 export { DaLabelBase };
