803d204ea06cdd10f35cd0ab6d78b6c3f98058d9
fanya3 <fanya3@gmail.com>
2020-06-26 18:10:37 +0200

    feature: added story

--- src/lib/FormControl/FormControl.stories.js
+++ src/lib/FormControl/FormControl.stories.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { withKnobs } from '@storybook/addon-knobs';
 import { folder, formStatusOptions } from '../../shared/constants';
+import FormControl from './FormControl';
 
 storiesOf(folder.form + 'FormControl', module)
     .addDecorator(withKnobs)
-    .add('Form Control', () => <FormControl></FormControl>);
+    .add('Form Control with input + message', () => <FormControl></FormControl>)
+    .add('Form Control with checkbox', () => <FormControl></FormControl>)
+
