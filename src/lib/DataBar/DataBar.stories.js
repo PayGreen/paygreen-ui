8537a61483c0ec4f64e9e69003efff68fb10455b
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-06-26 17:43:20 +0200

    feat: optimizing story for DataBar

--- src/lib/DataBar/DataBar.stories.js
+++ src/lib/DataBar/DataBar.stories.js
@@ -25,124 +25,78 @@ import {
 import DataBar from './DataBar';
 
 const defaultMaxValue = 100;
-const maxValueOptions = {
-    range: true,
-    min: 100,
-    max: 300,
-    step: 1,
-};
-
 const defaultValue = 50;
 const valueOptions = {
     range: true,
-    min: 0,
     max: 100,
-    step: 1,
 };
-
 const { wab, ...barColorPalletOptions } = colorPalletOptions;
-const colorPalletLabel = 'Color pallet';
-const colorStatusLabel = 'Status color';
-const colorThemeLabel = 'Theme color';
 
 storiesOf(folder.main + 'Databar', module)
     .addDecorator(withKnobs)
     .add('DataBar', () => (
         <DataBar
-            blockWidth={radios(
-                'Block width',
-                blockWidthOptions,
-                blockWidthDefault,
+            unit={text('Unity', '%')}
+            maxValue={number('Max value', defaultMaxValue)}
+            data={[
+                {
+                    value: number('Value', defaultValue, valueOptions),
+                    legend: ['compensé par ', <strong>vous</strong>],
+                    colorTheme: select(
+                        'Theme color',
+                        colorThemeOptions,
+                        colorThemeDefault,
+                    ),
+                    colorStatus: select(
+                        'Status color',
+                        formStatusOptions,
+                        formStatusDefault,
+                    ),
+                },
+            ]}
+            colorPallet={radios(
+                'Color pallet',
+                barColorPalletOptions,
+                colorPalletDefault,
             )}
-            hasBackground={boolean('With background', true)}
             backgroundColor={select(
                 'Background color',
                 greyOptions,
                 greyOptions.grey10,
             )}
+            hasBackground={boolean('With background', true)}
             hasDataLegend={boolean('With legend', true)}
             textSize={select('Text Size', fontSizeOptions, fontSizeDefault)}
-            unit={text('Unity', '%')}
-            colorPallet={radios(
-                colorPalletLabel,
-                barColorPalletOptions,
-                colorPalletDefault,
+            blockWidth={radios(
+                'Block width',
+                blockWidthOptions,
+                blockWidthDefault,
             )}
-            maxValue={number('Max value', defaultMaxValue, maxValueOptions)}
-            data={[
-                {
-                    value: number('Value', defaultValue, valueOptions),
-                    legend: ['compensé par', <strong> vous</strong>],
-                    colorStatus: select(
-                        colorStatusLabel,
-                        formStatusOptions,
-                        formStatusDefault,
-                    ),
-                    colorTheme: select(
-                        colorThemeLabel,
-                        colorThemeOptions,
-                        colorThemeDefault,
-                    ),
-                },
-            ]}
         />
     ))
     .add(
         'DataBar Multi',
         () => (
             <DataBar
-                blockWidth={radios(
-                    'Block width',
-                    blockWidthOptions,
-                    blockWidthDefault,
-                )}
-                hasBackground={boolean('With background', true)}
-                colorPallet={radios(
-                    colorPalletLabel,
-                    barColorPalletOptions,
-                    colorPalletDefault,
-                )}
                 data={[
                     {
                         value: number('Value 1', defaultValue, valueOptions),
-                        colorStatus: select(
-                            colorStatusLabel,
-                            formStatusOptions,
-                            formStatusDefault,
-                        ),
-                        colorTheme: select(
-                            colorThemeLabel,
-                            colorThemeOptions,
-                            colorThemeDefault,
-                        ),
+                        colorStatus: formStatusDefault,
+                        colorTheme: colorThemeDefault,
                     },
                     {
                         value: number('Value 2', defaultValue, valueOptions),
-                        colorStatus: select(
-                            'Status color 2',
-                            formStatusOptions,
-                            formStatusOptions.warning,
-                        ),
-                        colorTheme: select(
-                            'Theme color 2',
-                            colorThemeOptions,
-                            colorThemeOptions.tertiary,
-                        ),
+                        colorTheme: colorThemeOptions.tertiary,
+                        colorStatus: colorThemeDefault,
                     },
                     {
                         value: number('Value 3', defaultValue, valueOptions),
-                        colorStatus: select(
-                            'Status color 3',
-                            formStatusOptions,
-                            formStatusOptions.danger,
-                        ),
-                        colorTheme: select(
-                            'Theme color 3',
-                            colorThemeOptions,
-                            colorThemeOptions.quaternary,
-                        ),
+                        colorTheme: colorThemeOptions.quaternary,
+                        colorStatus: colorThemeDefault,
                     },
                 ]}
+                hasDataLegend={boolean('With legend', true)}
+                hasBackground={boolean('With background', true)}
             />
         ),
         {
