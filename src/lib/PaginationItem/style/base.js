3cd70cbf1ec731e178e2be0e117a3478e205456c
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-07-06 17:31:22 +0200

    fix: useless } in style

--- src/lib/PaginationItem/style/base.js
+++ src/lib/PaginationItem/style/base.js
@@ -1,24 +1,23 @@
 import { css } from 'styled-components';
 
 const activeStyle = css`
+    color: ${props => props.theme.wab.white00};
     background-image: linear-gradient(
         to bottom right,
         ${props => props.theme.color[props.colorTheme].gradientBase},
         ${props => props.theme.color[props.colorTheme].gradientShade}
     );
-
-    color: ${props => props.theme.wab.white00};
 `;
 
 const notActiveStyle = css`
+    color: ${props => props.theme.color[props.colorTheme].main};
     background-color: ${props => props.theme.color[props.colorTheme].light};
-    color :  ${props => props.theme.color[props.colorTheme].main};
 
     &:hover,
-    &:active {
+    &:active,
+    &:focus {
         ${activeStyle};
     }
-}
 `;
 
 export { activeStyle, notActiveStyle };
