5b7c1dd1d2f913d11a779c239bb00fda8eccf1d2
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-07-23 17:22:12 +0200

    feat: write test for Header

--- src/lib/Header/Header.test.js
+++ src/lib/Header/Header.test.js
@@ -1,13 +1,40 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import { ThemeDefault } from '../../theme';
+import Link from '../Link/Link';
+import MenuItem from '../MenuItem/MenuItem';
+import Menu from '../Menu/Menu';
+import MenuPrimary from '../MenuPrimary/MenuPrimary';
+import MenuHamburger from '../MenuHamburger/MenuHamburger';
+import MenuGroup from '../MenuGroup/MenuGroup';
 import Header from './Header';
 
 it('renders without crashing', () => {
     const component = TestRenderer.create(
         <Header theme={ThemeDefault}>
-            Something
-        </Header>
+            <MenuGroup theme={ThemeDefault}>
+                <div className="main-nav">
+                    <MenuHamburger theme={ThemeDefault} />
+                </div>
+
+                <MenuPrimary theme={ThemeDefault}>
+                    <Menu theme={ThemeDefault}>
+                        <a href="#">
+                            <MenuItem isMain={true} theme={ThemeDefault}>
+                                <Link
+                                    theme={ThemeDefault}
+                                    hasUnderline={false}
+                                    hasHover={false}
+                                    hasUppercase={true}
+                                >
+                                    About us
+                                </Link>
+                            </MenuItem>
+                        </a>
+                    </Menu>
+                </MenuPrimary>
+            </MenuGroup>
+        </Header>,
     );
     expect(component.toJSON()).toMatchSnapshot();
 });
