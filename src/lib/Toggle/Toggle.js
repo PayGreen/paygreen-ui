6fc439939501af23efed38e79888b7b5d54494ad
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-07-27 17:54:55 +0200

    feat: style Toggle

--- src/lib/Toggle/Toggle.js
+++ src/lib/Toggle/Toggle.js
@@ -1,24 +1,67 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {
-
+    colorPalletOptions,
+    colorThemeOptions,
+    colorThemeDefault,
+    greyOptions,
+    greyDefault,
+    formStatusOptions,
 } from '../../shared/constants';
 import { ToggleBase } from './style';
 
-const Toggle = (props) => {
-    return <ToggleBase
-        {...props}
-    >
-        {props.children}
-    </ToggleBase>;
+const Toggle = ({
+    id,
+    checkedLabel,
+    notCheckedLabel,
+    children,
+    colorPallet,
+    colorTheme,
+    colorWab,
+    colorStatus,
+    ...rest
+}) => {
+    return (
+        <ToggleBase
+            colorPallet={colorPallet}
+            colorTheme={colorTheme}
+            colorWab={colorWab}
+            colorStatus={colorStatus}
+        >
+            {children ? <span className="legend">{children}</span> : null}
+
+            <label htmlFor={id}>
+                <input type="checkbox" {...rest} id={id} />
+
+                <div className="toggle">
+                    <span className="checked-label">
+                        {checkedLabel ? checkedLabel : null}
+                    </span>
+
+                    <span className="not-checked-label">
+                        {notCheckedLabel ? notCheckedLabel : null}
+                    </span>
+                </div>
+            </label>
+        </ToggleBase>
+    );
 };
 
 Toggle.propTypes = {
-
+    id: PropTypes.string.isRequired,
+    checkedLabel: PropTypes.string,
+    notCheckedLabel: PropTypes.string,
+    colorPallet: PropTypes.oneOf(Object.values(colorPalletOptions)),
+    colorTheme: PropTypes.oneOf(Object.values(colorThemeOptions)),
+    colorWab: PropTypes.oneOf(Object.values(greyOptions)),
+    colorStatus: PropTypes.oneOf(Object.values(formStatusOptions)),
 };
 
 Toggle.defaultProps = {
-
+    colorPallet: colorPalletOptions.status,
+    colorTheme: colorThemeDefault,
+    colorWab: greyDefault,
+    colorStatus: formStatusOptions.success,
 };
 
 export default Toggle;
--- src/lib/Toggle/Toggle.stories.js
+++ src/lib/Toggle/Toggle.stories.js
@@ -1,20 +1,81 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean, text, radios, select } from '@storybook/addon-knobs';
+import { withKnobs, text, radios, select } from '@storybook/addon-knobs';
 import {
     folder,
+    colorPalletOptions,
+    colorThemeOptions,
+    colorThemeDefault,
+    greyOptions,
+    greyDefault,
+    formStatusOptions,
 } from '../../shared/constants';
 import Toggle from './Toggle';
+
+const colorThemeLabel = 'Color theme';
+const colorPalletLabel = 'Color pallet';
+const colorWabLabel = 'Grey color';
+const colorStatusLabel = 'Status color';
 
 storiesOf(folder.form + 'Toggle', module)
     .addDecorator(withKnobs)
     .add(
-        'Toggle',
+        'Toggle simple',
         () => (
             <Toggle
-                
+                id="toggle1"
+                colorPallet={radios(
+                    colorPalletLabel,
+                    colorPalletOptions,
+                    colorPalletOptions.status,
+                )}
+                colorTheme={select(
+                    colorThemeLabel,
+                    colorThemeOptions,
+                    colorThemeDefault,
+                )}
+                colorWab={select(
+                    colorWabLabel,
+                    greyOptions,
+                    greyDefault,
+                )}
+                colorStatus={select(
+                    colorStatusLabel,
+                    formStatusOptions,
+                    formStatusOptions.success,
+                )}
+            />
+        )
+    )
+    .add(
+        'Toggle with texts',
+        () => (
+            <Toggle
+                id="toggle2"
+                checkedLabel={text('Checked label', 'Yes')}
+                notCheckedLabel={text('Not checked label', 'No')}
+                colorPallet={radios(
+                    colorPalletLabel,
+                    colorPalletOptions,
+                    colorPalletOptions.status,
+                )}
+                colorTheme={select(
+                    colorThemeLabel,
+                    colorThemeOptions,
+                    colorThemeDefault,
+                )}
+                colorWab={select(
+                    colorWabLabel,
+                    greyOptions,
+                    greyDefault,
+                )}
+                colorStatus={select(
+                    colorStatusLabel,
+                    formStatusOptions,
+                    formStatusOptions.success,
+                )}
             >
-                Sample
+                Label text
             </Toggle>
         )
     );
