ea44c7013eceb83e55607ecc09e54bafd074b55f
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-07-28 16:13:25 +0200

    fix: change color status constant name in theme after rebase test: update snapshots

--- src/lib/Toggle/style/constants.js
+++ src/lib/Toggle/style/constants.js
@@ -3,24 +3,24 @@ const color = {
         base: {
             theme: props => props.theme.wab[props.colorWab],
             wab: props => props.theme.wab[props.colorWab],
-            status: props => props.theme.color.status.danger.main,
+            status: props => props.theme.status.danger.main,
         },
         checked: {
             theme: props => props.theme.color[props.colorTheme].main,
-            wab: props => props.theme.color.status[props.colorStatus].main,
-            status: props => props.theme.color.status[props.colorStatus].main,
+            wab: props => props.theme.status[props.colorStatus].main,
+            status: props => props.theme.status[props.colorStatus].main,
         },
     },
     bg: {
         base: {
             theme: props => props.theme.wab.grey10,
             wab: props => props.theme.wab.grey10,
-            status: props => props.theme.color.status.danger.light,
+            status: props => props.theme.status.danger.light,
         },
         checked: {
             theme: props => props.theme.color[props.colorTheme].light,
-            wab: props => props.theme.color.status[props.colorStatus].light,
-            status: props => props.theme.color.status[props.colorStatus].light,
+            wab: props => props.theme.status[props.colorStatus].light,
+            status: props => props.theme.status[props.colorStatus].light,
         },
     },
 };
