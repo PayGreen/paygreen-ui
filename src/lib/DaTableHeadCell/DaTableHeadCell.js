0a01aed08d5ca7a577d7f52db939dc5bee682e70
fanya3 <fanya3@gmail.com>
2020-08-03 14:23:01 +0200

    fix: fixed issue with children position in DaTableHeadCell component

--- src/lib/DaTableHeadCell/DaTableHeadCell.js
+++ src/lib/DaTableHeadCell/DaTableHeadCell.js
@@ -9,7 +9,7 @@ import {
 } from '../../shared/constants';
 import { DaTableHeadCellBase } from './style';
 
-const DaTableHeadCell = (props) => {
+const DaTableHeadCell = ({ children, label, ...rest }) => {
     const iconProps = {
         htmlTag: iconHtmlTagOptions.button,
         iconSize: iconSizeOptions.xxs,
@@ -22,29 +22,30 @@ const DaTableHeadCell = (props) => {
     let sortIcon = null;
     let groupIcon = null;
 
-    if (props.sortIcon) {
-        sortIcon = React.cloneElement(props.sortIcon, iconProps);
+    if (rest.sortIcon) {
+        sortIcon = React.cloneElement(rest.sortIcon, iconProps);
     }
 
-    if (props.groupIcon) {
-        groupIcon = React.cloneElement(props.groupIcon, iconProps);
+    if (rest.groupIcon) {
+        groupIcon = React.cloneElement(rest.groupIcon, iconProps);
     }
 
-    return <DaTableHeadCellBase
-        {...props}
-        hasControls={sortIcon || groupIcon}
-    >
-        {props.children}
+    return (
+        <DaTableHeadCellBase {...rest} hasControls={sortIcon || groupIcon}>
+            <span>{label}</span>
+            {sortIcon ? sortIcon : null}
+            {groupIcon ? groupIcon : null}
 
-        {sortIcon ? sortIcon : null}
-        {groupIcon ? groupIcon : null}
-    </DaTableHeadCellBase>;
+            {children}
+        </DaTableHeadCellBase>
+    );
 };
 
 DaTableHeadCell.propTypes = {
     isCheckbox: PropTypes.bool,
     sortIcon: PropTypes.element,
     groupIcon: PropTypes.element,
+    label: PropTypes.string,
 };
 
 DaTableHeadCell.defaultProps = {
--- src/lib/DaTableHeadCell/DaTableHeadCell.stories.js
+++ src/lib/DaTableHeadCell/DaTableHeadCell.stories.js
@@ -3,6 +3,7 @@ import { storiesOf } from '@storybook/react';
 import { withKnobs, boolean } from '@storybook/addon-knobs';
 import { folder, rotateSizeOptions } from '../../shared/constants';
 import { ArrowBottomIcon, MenuIcon } from '../Icon/Icon';
+import DaInput from '../DaInput/DaInput';
 import DaTableHeadCell from './DaTableHeadCell';
 
 const sortIconActiveLabel = 'Sort icon active';
@@ -48,7 +49,48 @@ storiesOf(folder.tables + folder.sub.daTable + 'DaTableHeadCell', module)
                     />
                 ) : null
             }
+            label="Cell for DaTableHead"
+        ></DaTableHeadCell>
+    ))
+    .add('DaTableHeadCell with children', () => (
+        <DaTableHeadCell
+            sortIcon={
+                boolean('With sort icon', false) ? (
+                    <ArrowBottomIcon
+                        isActive={boolean(sortIconActiveLabel, false)}
+                        rotateSize={
+                            boolean(ascSortLabel, false) &&
+                            boolean(sortIconActiveLabel, false)
+                                ? rotateSizeOptions.d180
+                                : rotateSizeOptions.d0
+                        }
+                        title={
+                            boolean(ascSortLabel, false) &&
+                            boolean(sortIconActiveLabel, false)
+                                ? 'Cancel sort on Sample'
+                                : 'Sort ' +
+                                  (boolean(sortIconActiveLabel, false)
+                                      ? 'ASC'
+                                      : 'DESC') +
+                                  ' on Sample'
+                        }
+                    />
+                ) : null
+            }
+            groupIcon={
+                boolean('With group icon', false) ? (
+                    <MenuIcon
+                        isActive={boolean(groupIconActiveLabel, false)}
+                        title={
+                            boolean(groupIconActiveLabel, false)
+                                ? 'Cancel group on Sample'
+                                : 'Group on Sample'
+                        }
+                    />
+                ) : null
+            }
+            label="Cell for DaTableHead"
         >
-            Cell for DaTableHead
+            <DaInput placeholder="Search" fieldSize="sm" />
         </DaTableHeadCell>
     ));
