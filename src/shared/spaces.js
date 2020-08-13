b7a8e9acc4443a3c9734fd618906a9869349708c
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-08-13 10:37:23 +0200

    doc: add js doc in spaces functions

--- src/shared/spaces.js
+++ src/shared/spaces.js
@@ -1,11 +1,28 @@
 import { css } from 'styled-components';
 import { stripUnit, math, directionalProperty } from 'polished';
 
+/**
+ * Calculate space with value in theme (space), value to remove and coeff
+ * 
+ * @param {string} space 
+ * @param {number} toRemove 
+ * @param {number} coeff 
+ * 
+ * @returns {string}
+ */
 const calculateSpace = (space, toRemove = 0, coeff = 1) => {
     return props =>
         math(props.theme.blockSpace[space] + '*' + coeff + '-' + toRemove);
 };
 
+/**
+ * Return correct space for specified screen
+ * 
+ * @param {string} screen 
+ * @param {string} space 
+ * 
+ * @returns {string}
+ */
 const blockSpace = (screen, space) => {
     switch (screen) {
         case 'sm':
@@ -27,6 +44,17 @@ const directionProperty = {
     lateral: 'Lateral',
 };
 
+/**
+ * Create CSS for responsive space attribute
+ * 
+ * @param {string} propAttribute margin or padding
+ * @param {number} toRemove value to remove to space
+ * @param {number} bottomCoeff coeff for bottom space
+ * @param {number} topCoeff coeff for top space
+ * @param {Array} directions which directions (top, right, left, bottom) create in css
+ * 
+ * @returns {string} css
+ */
 const responsiveSpaces = (
     propAttribute,
     toRemove = 0,
@@ -34,6 +62,18 @@ const responsiveSpaces = (
     topCoeff = 1,
     directions = Object.values(directionProperty),
 ) => {
+    /**
+     * Get correct space for given direction if exists
+     * 
+     * @param {string} screen sm, md
+     * @param {string} direction Top, Right, Left, Right, Lateral
+     * @param {number} toRemove value to remove to space
+     * @param {number} bottomCoeff coeff for bottom space
+     * @param {number} topCoeff coeff for top space
+     * @param {Array} directions all directions which be created in css
+     * 
+     * @returns {string|null}
+     */
     const getSpace = (
         screen,
         direction,
