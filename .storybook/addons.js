dcc63a188c1fbdd5eb5d9eaefafb64cca7dc12da
Olivia Gometz <olivia.gometz@paygreen.fr>
2020-10-05 14:33:10 +0200

    chore: update package json and remove deprecated notes

--- .storybook/addons.js
+++ .storybook/addons.js
@@ -2,7 +2,6 @@
 import '@storybook/addon-backgrounds/register';
 // https://www.npmjs.com/package/@storybook/addon-knobs
 import '@storybook/addon-knobs/register';
-import '@storybook/addon-notes/register-panel';
 import '@storybook/addon-viewport/register';
 import 'storybook-addon-styled-component-theme/dist/register';
 import '@storybook/addon-a11y/register';
