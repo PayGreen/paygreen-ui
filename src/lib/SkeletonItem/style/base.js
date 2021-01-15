c98a410fef753987b51f8f9ed9b2a4bcfe592d4f
Olivia Gometz <olivia.gometz@paygreen.fr>
2021-01-15 14:25:10 +0100

    feat: improve reflection effect in Skeleton for white and dark backgrounds

--- src/lib/SkeletonItem/style/base.js
+++ src/lib/SkeletonItem/style/base.js
@@ -4,6 +4,7 @@ import {
     skeletonItemTypeDefault,
     skeletonItemTypeOptions,
 } from '../../../shared/constants';
+import { reflectionGradient } from './constants';
 
 const childrenStyle = css`
     max-width: ${props =>
@@ -33,4 +34,102 @@ const loading = keyframes`
     }
 `;
 
-export { childrenStyle, noChildrenStyle, loading };
+const reflectionBase = css`
+    overflow: hidden;
+
+    &::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        top: 0;
+        left: 0;
+        transform: translateX(-100%);
+        animation: ${loading} ease-in-out 1.5s infinite;
+    }
+`;
+
+const reflection = {
+    white00: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.white20, 50)};
+        }
+    `,
+    white10: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.white00, 80)};
+        }
+    `,
+    white20: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.white00, 70)};
+        }
+    `,
+    grey10: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.white00)};
+        }
+    `,
+    grey20: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.grey10)};
+        }
+    `,
+    grey30: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.grey20)};
+        }
+    `,
+    grey40: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.grey30)};
+        }
+    `,
+    grey50: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.grey40)};
+        }
+    `,
+    grey60: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.grey50)};
+        }
+    `,
+    black00: css`
+        ${reflectionBase};
+
+        &::after {
+            background-image: ${props =>
+                reflectionGradient(props.theme.wab.grey50)};
+        }
+    `,
+};
+
+export { childrenStyle, noChildrenStyle, reflection };
