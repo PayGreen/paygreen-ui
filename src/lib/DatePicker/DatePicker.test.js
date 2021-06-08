e22c455734c01bbbb4270ab1db8037f49f9a259e
Olivia Gometz <olivia.gometz@paygreen.fr>
2021-06-08 15:34:43 +0200

    test: fix DatePicker value type

--- src/lib/DatePicker/DatePicker.test.js
+++ src/lib/DatePicker/DatePicker.test.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import moment from 'moment';
 import TestRenderer from 'react-test-renderer';
 import { ThemeDefault } from '../../theme';
 import DatePicker from './DatePicker';
@@ -8,7 +7,7 @@ it('renders without crashing', () => {
     const datePicker = TestRenderer.create(
         <DatePicker
             theme={ThemeDefault}
-            value={moment('2021-04-23T15:00:00.000')}
+            value="23/04/2021"
             onChange={() => {}}
         />,
     );
