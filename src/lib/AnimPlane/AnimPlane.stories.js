ab68e35c8f49c31e9bc8685dbfd5011a584849b6
Olivia Gometz <olivia.gometz@paygreen.fr>
2021-08-24 11:56:41 +0200

    fix: missing boolean on AnimPlane storie

--- src/lib/AnimPlane/AnimPlane.stories.js
+++ src/lib/AnimPlane/AnimPlane.stories.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, select } from '@storybook/addon-knobs';
+import { withKnobs, boolean, select } from '@storybook/addon-knobs';
 import {
     folder,
     colorThemeOptions,
@@ -13,11 +13,11 @@ storiesOf(folder.media + 'AnimPlane', module)
     .addDecorator(withKnobs)
     .add('AnimPlane', () => (
         <AnimPlane
+            isActive={boolean(labels.isActive, false)}
             colorTheme={select(
                 labels.colorTheme,
                 colorThemeOptions,
                 colorThemeDefault,
             )}
-            isActive={boolean(labels.isActive, false)}
         />
     ));
