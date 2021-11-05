8d756f8ad11cb511244e9f94e1963343d95868f5
Olivia Gometz <olivia.gometz@paygreen.fr>
2021-11-05 15:47:43 +0100

    doc: improve DaTable stories to show how active style and Checkboxes work together

--- src/lib/DaTableRow/DaTableRow.stories.js
+++ src/lib/DaTableRow/DaTableRow.stories.js
@@ -1,40 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { storiesOf } from '@storybook/react';
-import { withKnobs, boolean } from '@storybook/addon-knobs';
+import { withKnobs } from '@storybook/addon-knobs';
 import { folder } from '../../shared/constants';
-import labels from '../../shared/labels';
 import DaTableCell from '../DaTableCell/DaTableCell';
 import Checkbox from '../Checkbox/Checkbox';
 import DaTableRow from './DaTableRow';
 
 storiesOf(folder.table + folder.sub.daTable + 'DaTableRow', module)
     .addDecorator(withKnobs)
-    .add('DaTableRow', () => (
-        <DaTableRow isActive={boolean(labels.isActive, false)}>
-            <DaTableCell>
-                <Checkbox
-                    id="select"
-                    checked={boolean(labels.isActive, false)}
-                    readOnly={true}
-                />
-            </DaTableCell>
+    .add('DaTableRow', () => {
+        const [isActive, setActive] = useState(false);
 
-            <DaTableCell isId={true}>3456</DaTableCell>
+        return (
+            <DaTableRow isActive={isActive}>
+                <DaTableCell>
+                    <Checkbox
+                        id="select"
+                        onChange={() => setActive(!isActive)}
+                        checked={isActive}
+                    />
+                </DaTableCell>
 
-            <DaTableCell isMain={false} label="Date">
-                27/05/2020
-            </DaTableCell>
+                <DaTableCell isId={true}>3456</DaTableCell>
 
-            <DaTableCell>Marie Perez</DaTableCell>
+                <DaTableCell isMain={false} label="Date">
+                    27/05/2020
+                </DaTableCell>
 
-            <DaTableCell>34.56&nbsp;€</DaTableCell>
+                <DaTableCell>Marie Perez</DaTableCell>
 
-            <DaTableCell isMain={false} label="Type">
-                Cash
-            </DaTableCell>
+                <DaTableCell>34.56&nbsp;€</DaTableCell>
 
-            <DaTableCell isMain={false} label="A very long label">
-                A very long content to display
-            </DaTableCell>
-        </DaTableRow>
-    ));
+                <DaTableCell isMain={false} label="Type">
+                    Cash
+                </DaTableCell>
+
+                <DaTableCell isMain={false} label="A very long label">
+                    <span>A very long content without bold</span>
+                </DaTableCell>
+            </DaTableRow>
+        );
+    });
